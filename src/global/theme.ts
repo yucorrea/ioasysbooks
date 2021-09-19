85e036e4c391ad66e8d04e936829e01771d928fb
Yuri Correa <yuri.correa1@outlook.com>
2021-09-19 12:28:05 -0300

    style: confgure fonts and colors

---
+++ src/global/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: '#B22E6F',
+    primary_light: '#AB2680',
+
+    backgroud: '#FFFFFF',
+    border: 'rgba(51, 51, 51, 0.2)',
+
+    title: '#333333',
+    text: '#999999',
+
+    icon: '#333333',
+  },
+  fonts: {
+    light: 'Heebo-Light',
+    regular: 'Heebo-Regular',
+    medium: 'Heebo-Medium',
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { ThemeProvider } from 'styled-components';
 import { Routes } from './routes';
+
+import theme from './global/theme';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
