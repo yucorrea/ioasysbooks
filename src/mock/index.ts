9d23c57e26b2fe8d7e7031a24a187a95f868d2be
Yuri Correa <yuri.correa1@outlook.com>
2021-09-19 20:40:10 -0300

    chore: create mock for categories and years

---
+++ src/mock/index.ts
@@ -0,0 +1,21 @@
+const categories = [
+  { id: 1, value: 'Design' },
+  { id: 2, value: 'UX Design' },
+  { id: 3, value: 'UI Design' },
+  { id: 4, value: 'Arquitetura da Informação' },
+  { id: 5, value: 'CSS' },
+  { id: 6, value: 'Usabilidade' },
+  { id: 7, value: 'Design Thinking' },
+];
+
+const years = [
+  { id: 1, value: '2015' },
+  { id: 2, value: '2016' },
+  { id: 3, value: '2017' },
+  { id: 4, value: '2018' },
+  { id: 5, value: '2019' },
+  { id: 6, value: '2020' },
+  { id: 7, value: '2021' },
+];
+
+export { years, categories };
