2abc7816100b33c69fa1828069278d9258951e26
Yuri Correa <yuri.correa1@outlook.com>
2021-09-19 20:38:57 -0300

    fix: color key background

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -3,11 +3,12 @@ export default {
     primary: '#B22E6F',
     primary_light: '#AB2680',
 
-    backgroud: '#FFFFFF',
+    background: '#FFFFFF',
     border: 'rgba(51, 51, 51, 0.2)',
 
     title: '#333333',
     text: '#999999',
+    text_light: 'rgba(255,255,255,0.5)',
 
     icon: '#333333',
   },
