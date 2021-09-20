72b6b36cc0376d36a3164462d1605c049b3f9962
Yuri Correa <yuri.correa1@outlook.com>
2021-09-19 22:16:42 -0300

    chore: create IconButton component

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TouchableOpacityProps } from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import theme from '../../global/theme';
+
+import { Container } from './styles';
+
+interface Props extends TouchableOpacityProps {
+  onPress(): void;
+  icon: string;
+}
+
+export function IconButton({ onPress, icon, ...rest }: Props) {
+  return (
+    <Container {...rest} onPress={onPress}>
+      <Icon name={icon} size={16} color={theme.colors.title} />
+    </Container>
+  );
+}
