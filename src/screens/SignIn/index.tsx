bbbba9d01e4352eb4749040c2de370f7799cb4f4
Yuri Correa <yuri.correa1@outlook.com>
2021-09-19 22:18:16 -0300

    chore: create screen SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+
+import { Container, Wrapper, Logo, Title, Form } from './styles';
+
+import logo from './../../assets/Logo.png';
+
+import { Input } from '../../components/Input';
 
 export function SignIn() {
-  return <View />;
+  return (
+    <Container>
+      <Form>
+        <Wrapper>
+          <Logo source={logo} />
+          <Title>Books</Title>
+        </Wrapper>
+
+        <Input
+          label="Email"
+          placeholder="Seu e-mail"
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="email-address"
+        />
+        <Input
+          label="Senha"
+          secureTextEntry
+          placeholder="********"
+          enableButton
+          onPress={() => console.log('Entrou ...')}
+        />
+      </Form>
+    </Container>
+  );
 }
