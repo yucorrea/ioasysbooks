3d74d56a15203e494909ac4394c9ac3291a753e1
Yuri Correa <yuri.correa1@outlook.com>
2021-09-20 08:47:15 -0300

    chore: create mock for list options filters

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -9,13 +9,13 @@ const categories = [
 ];
 
 const years = [
-  { id: 1, value: '2015' },
-  { id: 2, value: '2016' },
-  { id: 3, value: '2017' },
-  { id: 4, value: '2018' },
-  { id: 5, value: '2019' },
-  { id: 6, value: '2020' },
-  { id: 7, value: '2021' },
+  { id: 1, value: 2015 },
+  { id: 2, value: 2016 },
+  { id: 3, value: 2017 },
+  { id: 4, value: 2018 },
+  { id: 5, value: 2019 },
+  { id: 6, value: 2020 },
+  { id: 7, value: 2021 },
 ];
 
 export { years, categories };
