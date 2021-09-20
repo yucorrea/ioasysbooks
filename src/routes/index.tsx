eb4f53c6ce8ae83a18ca5a17e7b5ae086837e41f
Yuri Correa <yuri.correa1@outlook.com>
2021-09-20 08:49:10 -0300

    chore: check user already logged

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { AppRoutes } from './app';
 import { AuthRoutes } from './auth';
 
 export function Routes() {
-  const isLogged = false;
+  const { token } = useSelector(state => state.auth);
+
+  const isLogged = token ? true : false;
 
   return isLogged ? <AppRoutes /> : <AuthRoutes />;
 }
