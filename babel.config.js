f60b9abb5770e5f7579f1e87cd651ca87326bbc2
Yuri Corrêa <yuri.correa@outlook.com>
2022-02-25 10:57:40 -0300

    feat: configuration path mapping

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,27 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@config': './src/config',
+          '@helpers': './src/helpers',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@store': './src/store',
+        },
+      },
+    ],
   ],
 };
